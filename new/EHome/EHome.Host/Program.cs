7864b542ae77726363e35634d703bcd4aed823e2
Dang Ngoc Tau <tau.dang@axonactive.vn>
2016-05-19 17:51:14 +0700

    test

--- new/EHome/EHome.Host/Program.cs
+++ new/EHome/EHome.Host/Program.cs
@@ -10,14 +10,15 @@ namespace EHome.Host
     {
         static void Main(string[] args)
         {
+            var container = TinyIoCContainer.Current;
+            container.Register<IAppSettings, AppSettings>().AsSingleton();
+            container.Register<IGateway, MqttGateway.MqttGateway>().AsSingleton();
+
             Console.Write("Starting http gateway...");
             var host = new Nancy.Hosting.Self.NancyHost(new Uri(ConfigurationManager.AppSettings["HostUri"]));
             host.Start();
             Console.WriteLine("started!");
 
-            var container = TinyIoCContainer.Current;
-            container.Register<IAppSettings, AppSettings>().AsSingleton();
-            container.Register<IGateway, MqttGateway.MqttGateway>().AsSingleton();
 
             // todo: start mqtt gateway
             Console.WriteLine("press any key to exit");
--- new/EHome/EHome.MqttGateway/MqttGateway.cs
+++ new/EHome/EHome.MqttGateway/MqttGateway.cs
@@ -1,6 +1,7 @@
 ﻿using EHome.Common;
 using EHome.Infrastructure;
 using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace EHome.MqttGateway
 {
@@ -19,7 +20,18 @@ namespace EHome.MqttGateway
         public void Start()
         {
             // todo: move to config.
+            _client.MqttMsgPublishReceived += _client_MqttMsgPublishReceived;
             _client.Connect("gateway");
+        }
+
+        public void RegisterEventAction()
+        {
+            // todo: IRequest, IReponse, ex: if event Turn on device 1, module Relay, PluginRelay is invoked.
+        }
+
+        private void _client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            // Get registered action in list, invoke that action.
         }
 
         public void Stop()
